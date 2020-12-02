8a53e56c1c2cb30e8747b26565e8b232e046949b
ye <juliet0727@126.com>
2020-12-03 00:10:08 +0800

    game

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,5 +1,6 @@
 import  React, { Component } from 'react';
 import { Row, Col } from 'antd'
+import 'antd/dist/antd.css';
 import banner1 from '../../source/game_pic/banner_pic/1.jpg'
 import banner2 from '../../source/game_pic/banner_pic/2.jpg'
 import banner3 from '../../source/game_pic/banner_pic/3.jpg'
@@ -7,15 +8,26 @@ import banner4 from '../../source/game_pic/banner_pic/4.jpg'
 import banner5 from '../../source/game_pic/banner_pic/5.jpg'
 import banner6 from '../../source/game_pic/banner_pic/6.jpg'
 import Header from "../header";
+import Footer from "../footer";
 
-
+import pic from '../../source/game_pic/content_pic/1.jpg'
 
 class Game extends Component {
 
     constructor(props){
         super(props);
         this.state={
-
+            gameName: "只狼",
+            publishTime: "2020/12/02",
+            publisher: "FromSoftware",
+            picUrl: '../../source/game_pic/content_pic/1.jpg',
+            labels: ["类魂游戏","困难","单人"],
+            price: 219,
+            description: "进入由打造了《黑暗之魂》系列的知名开发商FromSoftware倾力制作的全新冒险，用智慧和力量斩开复仇之路。 决死复仇，夺回荣誉，智杀强敌。",
+            discount: 0.2,
+            reviewNum: 10,
+            reviews: [{id: 1, gameId: 1, content: "评论1", userId: 1, userName: "玩家1", userPicUrl: "", time: ""}, {id: 2, gameId: 1, content: "评论2", userId: 2, userName: "玩家2", userPicUrl: "", time: ""}],
+            mvUrl: "",
         }
     }
 
@@ -30,11 +42,33 @@ class Game extends Component {
         //     }
         // }).catch( (error)=>{
         // })
-
     }
 
 
     render() {
+
+        // const picUrl = require(this.state.picUrl);
+
+        const showReviews =  this.state.reviews.length > 0
+            ?
+            this.state.reviews.map( (item,index,arr)=>{
+                //对返回的数据中的/n进行替换，替换成<br/>标签
+                const str = item.content.replace(/\n/g,'<br />');
+                //去除最后一条评论的borderbottom
+                const showBorder = index === arr.length-1 ? false : true;
+                return (
+                    <div className='newComment' style={{marginBottom:"25px"}}>
+                        <span className='avatar' style={{backgroundImage:`url(${item.userPicUrl})`}}></span>
+                        <span className='userComment'>
+                            <span style={{ color:"#1679C5",fontSize:"12px" }}>{item.userName}</span>
+                            <span style={{ fontSize:"12px" }} dangerouslySetInnerHTML={{__html: str}}></span>
+                        </span>
+                        <span className='commentTime'>{item.time}</span>
+                    </div>
+                )
+            })
+            :
+            null;
 
         return (
             <div>
@@ -42,168 +76,90 @@ class Game extends Component {
                     <Header />
                 </div>
 
-                <div className='song-content' style={ { marginTop:"20vh",minWidth:"1430px" } }>
+                <div style={{marginTop: "100px"}}>
                     <Row>
-                        <Col span={10}>
-                            <Row>
-                                <Col span={24}>
-                                    <div className='song-logo' style={{marginLeft: "100px"}}>
-                                        <div className='song-logo-img'>
-                                            <img  src={"http://cyq.center:8000/"+picUrl} alt="" />
-                                        </div>
-                                    </div>
-                                </Col>
-                            </Row>
-                            <Row style={{marginTop:"150px", marginLeft:"130px"}}>
-                                <div className="playText" onClick={()=>this.startPlaySong()} style={{marginRight:"15px"}}>
-                                    播放
+                        <Col span={12}>col-12</Col>
+                        <Col span={12}>col-12</Col>
+                    </Row>
+                    <Row>
+                        {/*图片或视频*/}
+                        <Col span={12}>
+                            <div className='song-logo' style={{marginLeft: "100px"}}>
+                                <div className='song-logo-img'>
+                                    <img src={pic} alt=""/>
                                 </div>
-                                <div className='playText' onClick={this.addToPlayList} style={{marginRight:"15px"}}>
-                                    添加
-                                </div>
-                                {
-                                    this.csrf
-                                        ?
-                                        <div className='playText' onClick={this.showCollectModal}>
-                                            收藏
-                                        </div>
-                                        :
-                                        null
-                                }
-                            </Row>
+                            </div>
                         </Col>
+                        {/*游戏简介*/}
                         <Col span={12} style={{ backgroundColor:"#fff", borderTop:"none",paddingBottom:"90px" }}>
-                            <Row>
-                                {/* //内容左侧 */}
-                                <div className='song-content-rightHeader'>
-                                    <span className='song-content-rightHeader-name'>{songName}</span>
-                                </div>
-                                <div className='song-content-singer' style={{marginTop:"20px"}}>
-                                    <span style={{ fontSize:"12px" }}>歌手:</span>
-                                    {
-                                        singer.singerName
-                                            ?
-                                            <span style={{ marginLeft:"5px", fontSize:"12px", color:"#2273C2" }}>{singer.singerName}</span>
-                                            :
-                                            null
-                                    }
-
-                                </div>
-                                <div className='song-content-belongAlbum'>
-                                    <span style={{ fontSize:"12px" }}>专辑:</span>
-                                    <span style={{marginLeft:"5px",fontSize:"12px",color:"#2273C2" }}>{album.albumName}</span>
-                                </div>
-                                <div className='song-content-label'>
-                                    <span style={{ fontSize:"12px" }}>标签:</span>
-                                    {label.map( (item) => {
-                                        return(
-                                            <span style={{marginLeft:"5px",fontSize:"12px",color:"#2273C2" }}>{item.name}</span>
-                                        )
-                                    })}
-                                </div>
-
-                                {/*歌词*/}
-                                {
-                                    this.state.isExtend
-                                        ?
-                                        <Extend html={html} handleExtendLyric={(boo)=>this.handleExtendLyric(boo)} />
-                                        :
-                                        <NotExtend html={html} handleExtendLyric={(boo)=>this.handleExtendLyric(boo)}/>
-                                }
-                            </Row>
+                            <div className='song-content-rightHeader'>
+                                <span className='song-content-rightHeader-name'>{this.state.gameName}</span>
+                            </div>
+                            <div className='song-content-singer' style={{marginTop:"20px"}}>
+                                <span style={{ fontSize:"12px" }}>发行商:</span>
+                                <span style={{ marginLeft:"5px", fontSize:"12px", color:"#2273C2" }}>{this.state.publisher}</span>
+                            </div>
+                            <div className='song-content-belongAlbum'>
+                                <span style={{ fontSize:"12px" }}>发行时间:</span>
+                                <span style={{marginLeft:"5px",fontSize:"12px",color:"#2273C2" }}>{this.state.publishTime}</span>
+                            </div>
+                            <div className='song-content-label'>
+                                <span style={{ fontSize:"12px" }}>标签:</span>
+                                {this.state.labels.map( (item) => {
+                                    return(
+                                        <span style={{marginLeft:"5px",fontSize:"12px",color:"#2273C2" }}>{item}</span>
+                                    )
+                                })}
+                            </div>
                         </Col>
-
-                        <Col span={2}></Col>
 
                     </Row>
 
-                    {
-                        this.csrf
-                            ?
-                            <div>
-                                {/*评论标题显示*/}
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={20}>
-                                        <Row>
-                                            <Col span={2}></Col>
-                                            <Col span={16}>
-                                                <div>
+                    {/*评论标题显示*/}
+                    <Row>
+                        <Col span={2}></Col>
+                        <Col span={20}>
+                            <Row>
+                                <Col span={2}></Col>
+                                <Col span={16}>
+                                    <div>
                                                 <span
                                                     style={{ color:"#4C4C4C",fontSize:"20px",marginRight:"20px" }}
                                                 >
                                                     评论
                                                 </span>
-                                                    <span
-                                                        style={{ color:"#6F6F6F",fontSize:"14px" }}
+                                        <span
+                                            style={{ color:"#6F6F6F",fontSize:"14px" }}
 
-                                                    >
-                                                    共{commentTotal}条评
+                                        >
+                                                    共{this.state.reviewNum}条评
                                                 </span>
-                                                </div>
-                                            </Col>
-                                            <Col span={6}></Col>
-                                        </Row>
+                                    </div>
+                                </Col>
+                                <Col span={6}></Col>
+                            </Row>
 
-                                    </Col>
-                                    <Col span={2}></Col>
-                                </Row>
+                        </Col>
+                        <Col span={2}></Col>
+                    </Row>
 
-                                {/*评论输入框*/}
-                                <Row style={{marginTop:"30px"}}>
-                                    <Col span={2}></Col>
-                                    <Col span={20}>
-                                        <Row>
-                                            <Col span={2}>
-                                                {/* <img src={defaultAvatar} width="30px" height="30px" alt=""/> */}
-                                            </Col>
-                                            <Col span={16} style={{ position:"relative" }}>
-                                                <TextArea rows={4} onChange={this.onInputChange} value={comment}/>
-                                                <span className='circle'>&nbsp;</span>
-                                                <span className='circle-mask'>&nbsp;</span>
-                                            </Col>
-                                            <Col span={6}></Col>
-                                        </Row>
-                                    </Col>
-                                    <Col span={2}></Col>
-                                </Row>
-
-
-                                <Row>
-                                    <Col span={16}></Col>
-                                    <Col span={8}>
-                                        <div className="commentText" onClick={this.handleCommentClick}>评论</div>
-                                    </Col>
-                                </Row>
-
-
-                                <Row style={{marginTop:"40px"}}>
-                                    <Col span={2}></Col>
-                                    <Col span={18}>
+                    <Row style={{marginTop:"40px"}}>
+                        <Col span={2}></Col>
+                        <Col span={18}>
+                            {
+                                this.state.reviews.length > 0
+                                    ?
+                                    <div style={{ marginLeft:"40px" }}>
                                         {
-                                            allComment.length > 0
-                                                ?
-                                                <div style={{ marginLeft:"40px" }}>
-                                                    <div style={{paddingBottom:"20px"}}>
-                                                        <span style={{ color:"#000",fontSize:"12px",fontWeight:"bold" }}>评论</span>
-                                                        <span style={{ color:"#000",fontSize:"12px",fontWeight:"bold" }}>({commentTotal})</span>
-                                                    </div>
-                                                    {
-                                                        renderComment
-                                                    }
-                                                </div>
-                                                :
-                                                null
+                                            showReviews
                                         }
-                                    </Col>
-                                    <Col span={4}></Col>
-                                </Row>
-
-                            </div>
-                            :
-                            null
-
-                    }
+                                    </div>
+                                    :
+                                    null
+                            }
+                        </Col>
+                        <Col span={4}></Col>
+                    </Row>
 
                 </div>
 
