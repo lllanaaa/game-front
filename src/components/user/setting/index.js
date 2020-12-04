059245589155f203d6c6f6faba5a7f0e7ced989a
Iristectorum-nosense <1410213902@qq.com>
2020-12-04 14:46:33 +0800

    登录，注册

---
+++ src/components/user/setting/index.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+
+class Setting extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+
+        return (
+            <div>个人设置</div>
+        )
+    }
+}
+
+export default Setting;
